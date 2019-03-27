c14922babc58f44d8032c9abc0aa5efddd0f043a
Prathibha Prasad <prathibhaprasad88@gmail.com>
2019-03-28 00:46:08 +0800

    trying to host site on GitHub repo

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Switch , Route} from 'react-router'
-import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux';
+import { Route} from 'react-router'
+import { HashRouter } from 'react-router-dom'
 import App from './App.js'
 
-render(<Provider>
-	<BrowserRouter>
-	  <Switch>
-        <Route exact path="/" component={App}/>
-     </Switch>
-    </BrowserRouter>
-	</Provider>, document.getElementById('root'));
+render(<HashRouter basename={process.env.PUBLIC_URL}>
+       <Route exact path="/" component={App}/>
+    </HashRouter>, document.getElementById('root'));
