75f587ee8cc538b4a742a3c83f7eb6fde907dd7c
Prathibha Prasad <prathibhaprasad88@gmail.com>
2019-03-28 00:58:59 +0800

    Publishing site

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Route} from 'react-router'
-import { HashRouter } from 'react-router-dom'
 import App from './App.js'
 
-render(<HashRouter basename={process.env.PUBLIC_URL}>
-       <Route exact path="/" component={App}/>
-    </HashRouter>, document.getElementById('root'));
+render(<App />, document.getElementById('root'));
